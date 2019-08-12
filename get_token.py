6a6c667eceb1c6e68e527bec22621977fc6eb226
AndrushaLOL <35956739+AndrushaLOL@users.noreply.github.com>
2019-08-12 20:21:04 +0500

    Security

--- get_token.py
+++ get_token.py
@@ -3,12 +3,6 @@ import json
 
 AUTH_URL = 'https://oauth.vk.com/token'
 
-payload = {
-    'grant_type': 'password',
-    'client_id': '2274003',
-    'client_secret': 'hHbZxrka2uZ6jB1inYsH',
-}
-
 
 def get_token(payload):
     r = requests.get(AUTH_URL, params=payload)
